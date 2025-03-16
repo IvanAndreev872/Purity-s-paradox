f91bf85e05f51af9d2f04bc013e3aab9eac792cc
Murtokov <MurtukovL@yandex.ru>
2025-03-16 19:58:02 +0300

    Added clone limitation

--- kursach/Assets/Scripts/lie/Controller.cs
+++ kursach/Assets/Scripts/lie/Controller.cs
@@ -10,6 +10,7 @@ public class Controller : MonoBehaviour, MovementInterface
     public float minimum_distance;
     public Transform room_center;
     public GameObject clone_prefab;
+    public int max_clones;
 
     private Rigidbody2D rb;
     private bool too_close = false;
@@ -76,10 +77,12 @@ public class Controller : MonoBehaviour, MovementInterface
 
         int real_position_index = Random.Range(0, positions_deflection.Length);
 
+        int clone_number = GameObject.FindGameObjectsWithTag("Clone").Length;
+
         for (int i = 0; i < positions_deflection.Length; i++)
         {
             Vector3 position = positions_deflection[i] + room_center.position;
-            if (i != real_position_index)
+            if (i != real_position_index && clone_number < max_clones)
             {
                 GameObject clone = Instantiate(clone_prefab, position, transform.rotation);
                 Controller clone_controller = clone.GetComponent<Controller>();
@@ -89,7 +92,10 @@ public class Controller : MonoBehaviour, MovementInterface
                     clone_controller.speed = speed;
                     clone_controller.minimum_distance = minimum_distance;
                     clone_controller.room_center = room_center;
+                    clone_controller.max_clones = max_clones;
+                    clone_controller.clone_prefab = clone_prefab;
                 }
+                clone_number++;
             }
             else 
             {
